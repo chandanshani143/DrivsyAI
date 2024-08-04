b9c97fa699221edd9f6bb1bfd43117ac04572466
Chandan Kumar Shani <chandankumarshani98@gmail.com>
2024-08-04 11:00:00 +0200

    created helper function to serialize carData, format currency and fixed bucket spelling

--- actions/cars.js
+++ actions/cars.js
@@ -186,7 +186,7 @@ export async function addCar({ carData, images }) {
       }
 
       // Get the public URL for the uploaded image
-      const publicUrl = `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/car-images/${filePath}`; // disable cache in config
+      const publicUrl = `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/cars-images/${filePath}`; // disable cache in config
 
       imageUrls.push(publicUrl);
     }
